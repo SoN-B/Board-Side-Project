e5807aaec62dc807cbb2a56dec345e903710460a
SON <wjdgh2777@naver.com>
2022-08-04 23:15:43 +0900

    Feat: refresh token 구현

--- FrontEnd/public/js/index.js
+++ FrontEnd/public/js/index.js
@@ -12,7 +12,16 @@ function profileAuth() {
     .then((res) => {
         if(res.code === 200) {
             location.href = "/user/profile/output?username="+res.data.username+"&email="+res.data.email;
-        } else {
+        } else if(res.message === "Token has expired."){ // 419
+            fetch("/user/token/refresh", {
+                headers: { 'authorization': localStorage.getItem('refresh_token') }
+                })
+                .then((res) => res.json())
+                .then((res) => {
+                    alert(res.message);
+                    localStorage.setItem('access_token', res.access_token);
+                })
+        } else { // 401
             alert(res.message);
             location.href = "/user/login";
         }
--- FrontEnd/public/js/post/create.js
+++ FrontEnd/public/js/post/create.js
@@ -25,6 +25,18 @@ function createPost() {
     .then((res) => {
         if(res.code === 200) {
             location.href = "/board";
-        } else return alert(res.message);
+        } else if(res.message === "Token has expired."){ // 419
+            fetch("/user/token/refresh", {
+                headers: { 'authorization': localStorage.getItem('refresh_token') }
+                })
+                .then((res) => res.json())
+                .then((res) => {
+                    alert(res.message);
+                    localStorage.setItem('access_token', res.access_token);
+                })
+        } else { // 401
+            alert(res.message);
+            location.href = "/user/login";
+        }
     })
 }
